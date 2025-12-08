581c064ed1f1672fc5c29a2ad938cd96dd1f6556
datlt <datlt244@gmail.com>
2025-12-08 14:39:40 +0700

    Initial commit

---
+++ common-lib/src/main/java/com/ecommerce/common/exception/AppException.java
@@ -0,0 +1,15 @@
+package com.ecommerce.common.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AppException extends RuntimeException{
+    private ErrorCode errorCode;
+
+    public AppException(ErrorCode errorCode){
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
---
+++ infra/init-postgres/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE order_service_db;
+CREATE DATABASE inventory_service_db;
+CREATE DATABASE user_service_db;
+CREATE DATABASE payment_service_db;
